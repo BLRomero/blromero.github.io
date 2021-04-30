d58ebd437f111bad0c8a4ef68b1a1f2152f8aff1
BLRomero <romerobonnette@byui.edu>
2021-04-30 14:18:42 -0500

    4/30/21 update

---
+++ lesson1/js/currentdate.js
@@ -0,0 +1,12 @@
+const options = {
+  weekday: 'long',
+  day: 'numeric',
+  month: 'long',
+  year: 'numeric'
+};
+document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
+
+
+var d = new Date();
+var n = d.getFullYear();
+document.querySelector('#year').innerHTML = n;
