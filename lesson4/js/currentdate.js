7226b7401712cf56cbe7f61b4f4fd0d2c0ceeb3a
BLRomero <romerobonnette@byui.edu>
2021-05-13 22:49:14 -0500

    done 5/13/21 1050pm

--- lesson4/js/currentdate.js
+++ lesson4/js/currentdate.js
@@ -1,12 +1,25 @@
-const options = {
-  weekday: 'long',
-  day: 'numeric',
-  month: 'long',
-  year: 'numeric'
-};
-document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
+// const options = {
+//   weekday: 'long',
+//   day: 'numeric',
+//   month: 'long',
+//   year: 'numeric'
+// };
+// document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
+
+
 
 
 var d = new Date();
 var n = d.getFullYear();
 document.querySelector('#year').innerHTML = n;
+
+
+
+const datefield = document.querySelector("#date");
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", {dateStyle:"full"}).format(now);
+
+// //long, medium, short, full
+datefield.innerHTML = `<em>${fulldate}</em>`;
+
+// console.log(new Intl.DateTimeFormat('en-US', { dateStyle: 'full', timeStyle: "long"}).format(now));
