112d3c8258c5d455c5d71d6c68960f1f6be1a9e3
BLRomero <romerobonnette@byui.edu>
2021-05-19 14:55:32 -0500

    social media

--- lesson5/js/bom.js
+++ lesson5/js/bom.js
@@ -4,36 +4,38 @@ const input = document.querySelector("input");
 const button = document.querySelector("button");
 
 // step5
-button.onclick = function(){
+button.addEventListener("click", function() {
   let myChapter = input.value;
   input.value = "";
 
-  const listChapter = document.
+  const listChapter = document;
 
-  // step6a
+  // step6a create an li element
   createElement('li');
   // const listText = document.createElement('span');
 
-   // step6b
+  // step6b create a delete button
   const listBtn = document.createElement('button');
 
-   // step6c
-   listChapter.appendChild(listText);
+  // step6c populate li elements TextCOntent or innerHTML with the input
+  listChapter.appendChild(listText);
   listText.textContent = myChapter;
   listChapter.appendChild(listBtn);
 
-  // fix to be a red x step 6d
+  // fix to be a red x step 6d populate the button textContent with an X
   listBtn.textContent = "Delete";
 
-// step6e????
+  // step6e???? append li element with the delete button
 
-  // step6f
+  // step6f append th elist element with the li element just created and appended with text and the delete button
   list.appendChild(listChapter);
 
-  // step 6g
-  listBtn.onclick = function(e){
+  // step 6g add an event listener to the button that removes the li element when clicked 
+  listBtn.addEventListener("click", function () {
     list.removeChild(listChapter);
-  }
-  // step 6h
+  })
+  // step 6h send the focus to the input element
   input.focus();
-}
+
+// step 6i  clean  up the successful add of a chapter by changing the input to nothing or the empty string and setting the focus to the input
+})
