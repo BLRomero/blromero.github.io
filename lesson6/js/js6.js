81b10561bd6bd0b73a999054d495d5604e8fa4d9
BLRomero <romerobonnette@byui.edu>
2021-05-28 09:35:33 -0500

    5/28/21

--- lesson6/js/js6.js
+++ lesson6/js/js6.js
@@ -18,29 +18,4 @@ var thedate = new Date();
 if (thedate.getDay() != 5) {
   document.querySelector('.banner').style.display = "none";
 }
-
-
-// Calculate the wind chill factor 
-function calculateWindChillFactor(temperature, windSpeed) {
-  return 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temperature * (Math.pow(windSpeed, 0.16)))
-}
-
-// const temp = 25,
-//       windSpeed = 10;
-
-// console.log(
-//   `The chill factor for temperature "${temp}" and wind speed "${windSpeed}" is "${calculateWindChillFactor(temp, windSpeed)}".`)
-
-document.querySelector(".currentTemp")
-document.querySelector(".wind")
-
-if (currentTemp <= 50) and (wind < 3) {
-    function calculateWindChillFactor(temperature, windSpeed) {
-    return 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temperature * (Math.pow(windSpeed, 0.16)))
-  }
-  // const temp = 25,
-//       windSpeed = 10;
-
-// console.log(
-//   `The chill factor for temperature "${temp}" and wind speed "${windSpeed}" is "${calculateWindChillFactor(temp, windSpeed)}".`)
-}
+  
