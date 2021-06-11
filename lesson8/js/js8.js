de4441ff7aec6ecfebefd59f0478e590404b8541
BLRomero <romerobonnette@byui.edu>
2021-06-11 17:35:47 -0500

    6/10/21 5:30

--- lesson8/js/js8.js
+++ lesson8/js/js8.js
@@ -90,3 +90,16 @@ localStorage.setItem('lastvisited', currentTime);
 
 
 // localStorage.countdays(document.querySelector().value);}
+
+
+
+function adjustRating(rating) {
+  document.getElementById("ratingvalue").innerHTML = rating;
+}
+function selectResponse() {
+	const s = document.querySelector('#selected')
+	const sel = document.querySelector('#selectbrowser');
+	s.style.display = "block";
+	s.textContent = sel.value;
+	
+}
