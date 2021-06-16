2d3cfd5715b7cb1679e3ce4a68eeadce481f5a58
BLRomero <romerobonnette@byui.edu>
2021-06-16 13:05:54 -0500

    final prophets

--- lesson9/js/prophet.js
+++ lesson9/js/prophet.js
@@ -2,38 +2,38 @@ const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-
 
 
 // feed required argument, 1st then() returns a promise 
-  // extract JSON content 2nd then() set up for us to work with the converted response
+// extract JSON content 2nd then() set up for us to work with the converted response
 fetch(requestURL)
   .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    console.table(jsonObject); // temporary checking for valid response and data parsing
     const prophets = jsonObject['prophets'];
-    for (let i = 0; i < prophets.length; i++ ) {
+    for (let i = 0; i < prophets.length; i++) {
 
       let card = document.createElement('section');
-      let prophetName = document.createElement('h2');
-      let birthDate = document.createElement('p');
-      let birthPlace = document.createElement('p');
-      let prophetImage = document.createElement('img');
-      
-      prophetName.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-      birthDate.textContent = 'Date of Birth: ' + prophets[i].birthdate;
-      birthPlace.textContent = "Place of Birth: "+ prophets[i].birthplace;
-      
-      prophetImage.setAttribute('src', prophets[i].imageurl);
+         
 
-      card.appendChild(prophetName);
-      card.appendChild(birthDate);
-      card.appendChild(birthPlace);
-      card.appendChild(prophetImage);
-      
+      let prophetName = document.createElement('h2');
+      prophetName.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+   
+      card.append(prophetName);
+
+      let birthDate = document.createElement('p');
+      birthDate.textContent = 'Date of Birth: ' + prophets[i].birthdate;
+      card.append(birthDate);
+
+      let birthPlace = document.createElement('p');
+      birthPlace.textContent = "Place of Birth: " + prophets[i].birthplace;
+      card.append(birthPlace);
+
+      let prophetImage = document.createElement('img');
+      prophetImage.setAttribute('src', prophets[i].imageurl);
+      prophetImage.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + ' -' + prophets[i].order) ;
+      card.append(prophetImage);
+       
+
       document.querySelector('div.cards').appendChild(card);
-  
     }
   });
-
- 
-
-
