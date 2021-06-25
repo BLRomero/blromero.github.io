56a9d7c8e022aea19e3f20824b85c76e9d5f0d14
BLRomero <romerobonnette@byui.edu>
2021-06-25 16:09:12 -0500

    6/25

--- lesson10/js/forecast.js
+++ lesson10/js/forecast.js
@@ -1,5 +1,7 @@
 // Preston
 
+
+
 const cityid = "5604473";
 const APPID = "17ae5c140fafbe607c5caf358df91955";
 // api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}
@@ -39,32 +41,46 @@ fetch(apiurl)
     document.getElementById('imagesrc').textContent = imagesrc;
     document.getElementById('icon').setAttribute('src', imagesrc);
  
+   
+    
+let day = 0;
+const dayofweek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat' ];
 
 
     const fivedayforecast = jsObject.list.filter((forecast) => forecast.dt_txt.includes("18:00:00"));
     console.log(fivedayforecast);
 
-    for (let i = 0; i < fivedayforecast.length; i++) {
-      let forecast = fivedayforecast[i];
-      let tempElement = "temp" + i;
-      let iconElement = "icon" + i;
-      let dayElement = "day" + i;
-      let forecastDate = new Date(forecast.dt_txt);
-      let forecastDay = days[forecastDate.getDay()];
 
-      const desc = forecast.weather[0].description;
-      const iconsrc = "https://openweathermap.org/img/w/" + forecast.weather[0].icon + ".png";
+    fivedayforecast.forEach (x =>{
+      let d = new Date (x.dt_txt);
+      console.log(d);
 
-      document.getElementById(dayElement).textContent = forecastDay;
-      document.getElementById(iconElement).setAttribute("src", iconsrc);
-      document.getElementById(iconElement).setAttribute("alt", desc);
-      document.getElementById(tempElement).textContent = forecast.main.temp;
+      document.getElementById(`dayofweek${day +1}`).textContent = dayofweek[d.getDay()];
+      document.getElementById(`forecast${day+1}`).textContent= x.main.temp.toFixed();
+    })
+    // for (let i = 0; i < fivedayforecast.length; i++) {
+    //   let forecast = fivedayforecast[i];
+    //   let tempElement = "temp" + i;
+    //   let iconElement = "icon" + i;
+    //   let dayElement = "day" + i;
+    //   let forecastDate = new Date(forecast.dt_txt);
+    //   let forecastDay = days[forecastDate.getDay()];
 
-    }});
+    //   const desc = forecast.weather[0].description;
+    //   const iconsrc = `https:openweathermap.org/img/w/${jsObject.icon}.png`;
+      
+    //   console.log(iconsrc);
+
+    //   document.getElementById(dayElement).textContent = forecastDay;
+    //   document.getElementById(iconElement).setAttribute("src", iconsrc);
+    //   document.getElementById(iconElement).setAttribute("alt", desc);
+    //   document.getElementById(tempElement).textContent = forecast.main.temp;
+
+    // }});
 
 
 
-// // Fish Haven
+// Fish Haven
 // const cityid = "5585010";
 // const APPID = "17ae5c140fafbe607c5caf358df91955";
 // // api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}
@@ -78,29 +94,30 @@ fetch(apiurl)
 
 //     const temperature = document.querySelector('.currentTemp');
 //     temperature.textContent = jsObject.list[0].main.temp.toFixed(0);
-//     // console.log(temperature);
+//     console.log(temperature);
 
 //     const high = document.querySelector('.high');
 //     high.textContent = jsObject.list[0].main.temp_max.toFixed(0);
-//     //  console.log(high);
+//      console.log(high);
 
 //     const wind = document.querySelector('.wind');
 //     wind.textContent = jsObject.list[0].wind.speed.toFixed(0);
-//     // console.log(wind);
+//     console.log(wind);
 
 //     const humidity = document.querySelector('.humidity');
 //     humidity.textContent = jsObject.list[0].main.humidity;
-//     // console.log(humidity);
+//     console.log(humidity);
+
 //     const currently = document.querySelector('#currently');
 //     currently.innerHTML = `<strong>${jsObject.list[0].weather[0].description.toUpperCase()}</strong>`;
-//     // console.log(currently);
+//     console.log(currently);
 
 
 //     const imagesrc = `https://openweathermap.org/img/w/${jsObject.list[0].weather[0].icon}.png`;
-//     // console.log(imagesrc);
+//     console.log(imagesrc);
 
 //     const desc = jsObject.list[0].weather[0].description;
-//     // console.log(desc);
+//     console.log(desc);
 
 //     document.getElementById('imagesrc').textContent = imagesrc;
 //     document.getElementById('icon').setAttribute('src', imagesrc);
@@ -148,4 +165,4 @@ fetch(apiurl)
 //     document.getElementById('imagesrc').textContent = imagesrc;
 //     document.getElementById('icon').setAttribute('src', imagesrc);
  
-// })
+});
