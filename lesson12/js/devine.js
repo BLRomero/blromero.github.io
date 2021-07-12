fe206acc118e83002eb288b73f3c72e58ba07a79
BLRomero <romerobonnette@byui.edu>
2021-07-12 09:30:17 -0500

    7/12 start this week

--- lesson12/js/devine.js
+++ lesson12/js/devine.js
@@ -3,9 +3,13 @@ const hambutton = document.querySelector(".ham");
 
 const mainnav = document.querySelector(".navigation");
 
-hambutton.addEventListener('click', () => {mainnav.classList.toggle("responsive")}, false);
+hambutton.addEventListener('click', () => {
+  mainnav.classList.toggle("responsive")
+}, false);
 
-window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove("responsive")};
+window.onresize = () => {
+  if (window.innerWidth > 760) mainnav.classList.remove("responsive")
+};
 
 
 
@@ -31,7 +35,7 @@ document.getElementById('currentdate').textContent = new Date().toLocaleDateStri
 const latitude = '29.14';
 const longitude = '-98.9053';
 
-const APIID ='bedda6c15123f002141cc2835427f5c0'
+const APIID = 'bedda6c15123f002141cc2835427f5c0'
 
 const apiurl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,daily&appid=${APIID}&units=imperial`;
 
@@ -39,7 +43,7 @@ fetch(apiurl)
   .then((response) => response.json())
   .then((jsObject) => {
     // console.log(jsObject);
-      
+
     const temperature = document.querySelector('.currentTemp');
     temperature.textContent = jsObject.current.temp.toFixed(0);
     // console.log(temperature);
@@ -48,12 +52,12 @@ fetch(apiurl)
     description.innerHTML = `<strong>${jsObject.current.weather[0].description.toUpperCase()}</strong>`;
     // console.log(currently);
 
-    
+
 
     const humidity = document.querySelector('.humidity');
     humidity.textContent = jsObject.current.humidity.toFixed(0);
     // console.log(humidity);
-    
+
 
     const imagesrc = `https://openweathermap.org/img/w/${jsObject.current.weather[0].icon}.png`;
     // console.log(imagesrc);
@@ -63,112 +67,206 @@ fetch(apiurl)
   });
 
 
-//3 Day Forecast
+//3 Day Forecast  ***********NEED TO FIX**********
 // const lat = '29.14';
 // const lon = '-98.9053';
 
 // APIID ='bedda6c15123f002141cc2835427f5c0'
 
-const apiurl3 = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly&appid=${APIID}&units=imperial`;
+// const apiurl3 = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=current,minutely,hourly&appid=${APIID}&units=imperial`;
 
-fetch(apiurl3)
-  .then((response) => response.json())
-  .then((jsObject) => {
-    // console.log(jsObject);
+// fetch(apiurl3)
+//   .then((response) => response.json())
+//   .then((jsObject) => {
+//     // console.log(jsObject);
 
-    let day = 0;
-    const dayofweek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+//     let day = 0;
+//     const dayofweek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-    // const threedayforecast = jsObject.list.filter((forecast) => forecast.dt_txt.includes("18:00:00"));
-    // console.log(threedayforecast); 
+//     const daily = jsObject.daily;
+//     console.log(daily);
 
-    // threedayforecast.forEach(x => {
-    //   let d = new Date(x.dt_txt);
-    //   day++;
-      // console.log(d);
+//     for (let i=0; i < daily.length; i++){
+     
+//       document.getElementById(`dayofweek${day +1}`).textContent = dayofweek[d.getDay()];
+//       document.getElementById(`forecast${day +1}`).textContent = i.temp.max.toFixed();
 
-
-      // document.getElementById(`dayofweek${day +1}`).textContent = dayofweek[d.getDay()];
-      // document.getElementById(`forecast${day +1}`).textContent = x.main.temp.toFixed();
-
-      // document.getElementById(`icon${day +1}`).src ="https://openweathermap.org/img/w/" + x.weather[0].icon + ".png";
- 
-
-      const temperature = document.querySelector('.temp');
-      temperature.textContent = jsObject.current.temp.toFixed(0);
-      // console.log(temperature);
-  
-      const description = document.querySelector('.description');
-      description.innerHTML = `<strong>${jsObject.current.weather[0].description.toUpperCase()}</strong>`;
-      // console.log(currently);  
-  
-      const imagesrc = `https://openweathermap.org/img/w/${jsObject.current.weather[0].icon}.png`;
-      // console.log(imagesrc);
-  
-      document.getElementById('icon').setAttribute('src', imagesrc);
+//       document.getElementById(`icon${day +1}`).src ="https://openweathermap.org/img/w/" + weather[0].icon + ".png";
+    
+      
+//     }});
 
 
-    })
-  });
-
-  
+// business directory
 const jsonfile = "data/devine.json"
 
 fetch(jsonfile)
-.then(function (response) {
-  return response.json();
-})
-.then(function (jsonObject) {
-  console.table(jsonObject);
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    // console.table(jsonObject);
 
-  const businesses =jsonObject['businesses'];
-  
-  for(let i = 0; i < businesses.length; i++){
-    let card = document.createElement('section');
+    const businesses = jsonObject['businesses'];
 
-    let name = document.createElement('h2');
-    name.textContent = businesses[i].name;
-    card.append(name);
+    for (let i = 0; i < businesses.length; i++) {
+      let card = document.createElement('section');
 
-    let purpose = document.createElement('p');
-    purpose.textContent = businesses[i].purpose;
-    card.append(purpose);
+      let name = document.createElement('h2');
+      name.textContent = businesses[i].name;
+      card.append(name);
 
-    let address = document.createElement('p');
-    address.textContent = businesses[i].address;
-    card.append(address);
+      let purpose = document.createElement('p');
+      purpose.textContent = businesses[i].purpose;
+      card.append(purpose);
 
-    let city = document.createElement('p');
-    city.textContent = businesses[i].city + ',  ' + businesses[i].state + ' ' + businesses[i].zip;
-    card.append(city);
+      let address = document.createElement('p');
+      address.textContent = businesses[i].address;
+      card.append(address);
 
-    let phonenumber = document.createElement('p');
-    phonenumber.textContent = businesses[i].phonenumber;
-    card.append(phonenumber);
+      let city = document.createElement('p');
+      city.textContent = businesses[i].city + ',  ' + businesses[i].state + ' ' + businesses[i].zip;
+      card.append(city);
 
-    let website = document.createElement('p');
-    website.textContent = businesses[i].website;
-    card.append(website);
+      let phonenumber = document.createElement('p');
+      phonenumber.textContent = businesses[i].phonenumber;
+      card.append(phonenumber);
 
-    document.querySelector("div.cards").appendChild(card);
-  }
+      let website = document.createElement('p');
+      website.textContent = businesses[i].website;
+      card.append(website);
+
+      document.querySelector("div.cards").appendChild(card);
+    }
   });
 
 
- 
-       
+// STAR BUSINESS 1
+const jsonfile1 = "data/devine.json"
+
+fetch(jsonfile1)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    // console.table(jsonObject);
+
+    const businesses = jsonObject['businesses'];
+
+    let stars = document.createElement('section');
+    let starsinfo = document.createElement('div');
+
+    let h2 = document.createElement('h2');
+    h2.textContent = businesses[0].name;
+
+
+    let p1 = document.createElement('p');
+    p1.textContent = businesses[0].address;
+
+    let p2 = document.createElement('p');
+    p2.textContent = businesses[0].city + ',  ' + businesses[4].state + ' ' + businesses[0].zip;
+
+
+    let p3 = document.createElement('p');
+    p3.textContent = businesses[0].phonenumber;
+
+    let p4 = document.createElement('p');
+    p4.textContent = businesses[0].website;
+
+    starsinfo.append(h2, p1, p2, p3, p4);
+    starsinfo.setAttribute('class', stars);
+
+    document.querySelector('div.starsinfo').appendChild(starsinfo);
+  });
+
+// STAR BUSINESS 2
+const jsonfile2 = "data/devine.json"
+
+fetch(jsonfile2)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    // console.table(jsonObject);
+
+    const businesses = jsonObject['businesses'];
+
+    let stars = document.createElement('section');
+    let starsinfo = document.createElement('div');
+
+    let h2 = document.createElement('h2');
+    h2.textContent = businesses[3].name;
+
+
+    let p1 = document.createElement('p');
+    p1.textContent = businesses[3].address;
+
+    let p2 = document.createElement('p');
+    p2.textContent = businesses[3].city + ',  ' + businesses[4].state + ' ' + businesses[3].zip;
+
+
+    let p3 = document.createElement('p');
+    p3.textContent = businesses[3].phonenumber;
+
+    let p4 = document.createElement('p');
+    p4.textContent = businesses[3].website;
+
+    starsinfo.append(h2, p1, p2, p3, p4);
+    starsinfo.setAttribute('class', stars);
+
+    document.querySelector('div.starsinfo').appendChild(starsinfo);
+  });
+
+// STAR BUSINESS 3
+const jsonfile3 = "data/devine.json"
+
+fetch(jsonfile3)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    // console.table(jsonObject);
+
+    const businesses = jsonObject['businesses'];
+
+    let stars = document.createElement('section');
+    let starsinfo = document.createElement('div');
+
+    let h2 = document.createElement('h2');
+    h2.textContent = businesses[4].name;
+
+
+    let p1 = document.createElement('p');
+    p1.textContent = businesses[4].address;
+
+    let p2 = document.createElement('p');
+    p2.textContent = businesses[4].city + ',  ' + businesses[4].state + ' ' + businesses[4].zip;
+
+
+    let p3 = document.createElement('p');
+    p3.textContent = businesses[4].phonenumber;
+
+    let p4 = document.createElement('p');
+    p4.textContent = businesses[4].website;
+
+    starsinfo.append(h2, p1, p2, p3, p4);
+    starsinfo.setAttribute('class', stars);
+
+    document.querySelector('div.starsinfo').appendChild(starsinfo);
+  });
+
 
 // count
 
 // For use in converting milliseconds to seconds
 const ONESECOND = 1000;
-const  convert = 60*60*24* ONESECOND;
+const convert = 60 * 60 * 24 * ONESECOND;
 // Obtain current time
 let today = new Date();
 let currentTime = today.getTime();
 
 // If there is no "lastvisited" data in local storage, set it to current time
-if(!localStorage.getItem('lastvisited')) {
+if (!localStorage.getItem('lastvisited')) {
   localStorage.setItem('lastvisited', currentTime);
 }
 
@@ -176,7 +274,7 @@ if(!localStorage.getItem('lastvisited')) {
 var lastTime = parseInt(localStorage.getItem('lastvisited'));
 
 // Subtract current time with last-visited time and convert to seconds
-var days = (currentTime - lastTime) /convert;
+var days = (currentTime - lastTime) / convert;
 
 // // Display to the console how many seconds since last page visit
 // console.log("You last visited " + days.toFixed(0) + "  days ago.");
@@ -192,7 +290,7 @@ const images = document.querySelectorAll('img[data-src]');
 
 function preloadImage(img) {
   const src = img.getAttribute("data-src");
-  if(!src) {
+  if (!src) {
     return;
   }
 
@@ -205,14 +303,14 @@ const imgOptions = {
 };
 
 const imgObserver = new IntersectionObserver((entries, imgObserver) => {
-entries.forEach(entry => {
-  if(!entry.isIntersecting) {
-  return;
-} else {
-  preloadImage(entry.target);
-  imgObserver.unobserve(entry.target);
-}
-});
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      preloadImage(entry.target);
+      imgObserver.unobserve(entry.target);
+    }
+  });
 }, imgOptions);
 
 images.forEach(image => {
